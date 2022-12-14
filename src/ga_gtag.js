4e2502e03cb788775ad0110794c5f0f021f79b8c
Raman Siliuk <rsiluk@swiftinvention.com>
2022-12-14 18:39:13 +0400

    small fix

--- src/ga_gtag.js
+++ src/ga_gtag.js
@@ -5,6 +5,11 @@ export const install = (trackingId, additionalConfigInfo = {}) => {
     if (document.getElementById(scriptId)) return;
 
     const {head, body} = document;
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MFQH35F')
     // const script = document.createElement('script');
     // script.id = scriptId;
     // script.type = 'text/javascript';
@@ -12,12 +17,12 @@ export const install = (trackingId, additionalConfigInfo = {}) => {
     // script.src = `https://www.googletagmanager.com/gtag/js?id=${trackingId}`;
     // head.insertBefore(script, head.firstChild);
 
-    const script2 = document.createElement('script');
-    script2.id = script2Id;
-    script2.type = 'text/javascript';
-    script2.async = true;
-    script2.src = `https://www.googletagmanager.com/gtm.js?id=GTM-MFQH35F`;
-    head.insertBefore(script2, head.firstChild);
+    // const script2 = document.createElement('script');
+    // script2.id = script2Id;
+    // script2.type = 'text/javascript';
+    // script2.async = true;
+    // script2.src = `https://www.googletagmanager.com/gtm.js?id=GTM-MFQH35F`;
+    // head.insertBefore(script2, head.firstChild);
 
     const noscript = document.createElement('noscript');
     const iframe = document.createElement('iframe');
@@ -26,13 +31,15 @@ export const install = (trackingId, additionalConfigInfo = {}) => {
     iframe.style = 'display:none;visibility:hidden';
     iframe.src = `https://www.googletagmanager.com/ns.html?id=GTM-MFQH35F`;
     noscript.insertBefore(iframe, noscript.firstChild);
+    console.log('1', noscript)
     body.insertBefore(noscript, body.firstChild);
 
-    window.dataLayer = window.dataLayer || [];
+
+    // window.dataLayer = window.dataLayer || [];
 
     // gtag('js', new Date());
     // gtag('config', trackingId, additionalConfigInfo);
-    gtag({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+    // gtag({'gtm.start': new Date().getTime(), event: 'gtm.js'});
 };
 
 export const gtag = function() {
